0e5c9aa624fad51355a22b33bfd7a64a7c91f808
Kartick Karmakar <34238983+kartick14@users.noreply.github.com>
2018-02-06 13:18:18 +0530

    Update ajax-add-to-cart.js

--- ajax-add-to-cart.js
+++ ajax-add-to-cart.js
@@ -1,3 +1,24 @@
+/*
+
+{% assign lineval = 0 %}
+{% for line_item in cart.items %}
+  {% if line_item.variant_id == variantid.id %}
+    {% assign lineval = line_item.quantity %}
+  {% endif %}
+{% endfor %}
+ 
+<div class="cart-quantity">
+  <input type='button' value='-' class='qtyminus' field='addcart_{{ variantid.id }}' />
+  <input type="number" name="updates[]" id="addcart_{{ variantid.id }}" class="quantity" value="{{lineval}}" />
+  <input type='button' value='+' class='qtyplus' field='addcart_{{ variantid.id }}' />
+</div>
+<div class="continue_shopping animated fadeInUp" id="text_{{ variantid.id }}" style="display: none;">
+  <a href="javascript:void(0);" class="close cls_msg">Continue Shopping</a> or <a href="/cart">View Cart</a>
+</div>
+
+*/
+
+
 jQuery(document).ready(function(){
   // This button will increment the value
   $('.qtyplus').click(function(e){
