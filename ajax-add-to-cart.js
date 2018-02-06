00cf1b77b40dbf4c8f1dde5c7822858c0a1692ec
Kartick Karmakar <34238983+kartick14@users.noreply.github.com>
2018-02-06 13:13:49 +0530

    Create ajax-add-to-cart.js

---
+++ ajax-add-to-cart.js
@@ -0,0 +1,108 @@
+jQuery(document).ready(function(){
+  // This button will increment the value
+  $('.qtyplus').click(function(e){
+    // Stop acting like a button
+    e.preventDefault();
+    // Get the field name
+    fieldName = $(this).attr('field');
+    // Get its current value
+    var currentVal = parseInt($('input[id='+fieldName+']').val());    
+    // If is not undefined
+    if (!isNaN(currentVal)) {
+      // Increment
+      $('input[id='+fieldName+']').val(currentVal + 1);
+      var cartVal = parseInt($('input[id='+fieldName+']').val());
+      var prodid = fieldName.split('_');
+      addtocart(cartVal,prodid[1]);
+    } else {
+      // Otherwise put a 0 there
+      $('input[id='+fieldName+']').val(0);
+    }
+  });
+  // This button will decrement the value till 0
+  $(".qtyminus").click(function(e) {
+    // Stop acting like a button
+    e.preventDefault();
+    // Get the field name
+    var fieldName = $(this).attr('field');    
+    // Get its current value
+    var currentVal = parseInt($('input[id='+fieldName+']').val());    
+    // If it isn't undefined or its greater than 0
+    if (!isNaN(currentVal) && currentVal > 0) {
+      // Decrement one
+      $('input[id='+fieldName+']').val(currentVal - 1);
+      var cartVal = parseInt($('input[id='+fieldName+']').val());
+      var prodid = fieldName.split('_');
+      removefromcart(cartVal,prodid[1]);
+    } else {
+      // Otherwise put a 0 there
+      $('input[id='+fieldName+']').val(0);
+    }
+  });
+  
+  $('.cls_msg').click(function(){
+    $('.continue_shopping').hide();
+  });
+  
+});  
+
+
+function addtocart(cartVal,prodid){  
+  Shopify.queue = [];    
+  Shopify.queue.push({
+    variantId: prodid,
+    variantQty: cartVal,
+  });  
+
+  Shopify.moveAlong = function() {    
+    if (Shopify.queue.length) {
+      var request = Shopify.queue.shift();      
+      $.ajax({
+        type: 'POST',
+        url: '/cart/add.js',
+        dataType: 'json',
+        data: {quantity: 1, id: request.variantId},
+        success: function(res){
+          Shopify.moveAlong();
+          $.getJSON( "/cart.js", function( data ) {
+            console.log(data);
+            $('.continue_shopping').hide();
+            $('#text_'+request.variantId).show();
+            $('.cart_button span.cart_count').remove();
+            $('.cart_button').append('<span class="cart_count"> ('+data.item_count+')</span>');
+          });          
+        },
+        error: function(){          
+          if (Shopify.queue.length){
+            Shopify.moveAlong()
+          } else {            
+            $('#text_'+request.variantId).hide();          	
+          }
+        }
+      });
+    }   
+  };
+  Shopify.moveAlong();
+};
+
+function removefromcart(qty,prodid){  console.log('qty1:'+qty);
+  /*if (qty <= 1){
+    qty = 0;
+  }else{
+    qty = qty;
+  }*/
+  console.log('qty2:'+qty);
+  var params = {
+    type: 'POST',
+    url: '/cart/change.js',
+    data: 'quantity=' + qty + '&id=' + prodid,
+    dataType: 'json',
+    success: function(cartdata) { 
+      $('.continue_shopping').hide();
+      $('#text_'+prodid).show();
+      $('.cart_button span.cart_count').remove();
+      $('.cart_button').append('<span class="cart_count"> ('+cartdata.item_count+')</span>');
+    }
+  };
+  $.ajax(params);
+}
